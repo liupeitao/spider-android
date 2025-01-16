9be69d64457b3ca7cd8d2e92aae6e29c0286db84
PTliu <sudolovelnpctly6869@outlook.com>
2025-01-16 14:19:56 +0800

    优化deco_log_state装饰器中的状态记录逻辑，确保在函数执行前后正确设置和记录状态

--- core/tools/deco.py
+++ core/tools/deco.py
@@ -15,10 +15,7 @@ def deco_log_state(state: StateEnum):
 
             spider: Spider = args[0]
             log_item = LogModel(app=spider.app, func=func, state=state, spider=spider)
-            context = StateContext()
-            
-            context.set_state(state)
-            log_state(context, log_item, *args, **kwargs)
+            context = StateContext()            
             try:
                 result = await func(*args, **kwargs)
             except Exception as e:
@@ -28,6 +25,8 @@ def deco_log_state(state: StateEnum):
                 log_state(context, log_item, *args, **kwargs)
                 raise e
             else:
+                context.set_state(state)
+                log_state(context, log_item, *args, **kwargs)
                 return result
 
         @functools.wraps(func)
@@ -38,8 +37,6 @@ def deco_log_state(state: StateEnum):
             spider: Spider = args[0]
             log_item = LogModel(app=spider.app, func=func, state=state, spider=spider)
             context = StateContext()
-            context.set_state(state)
-            log_state(context, log_item, *args, **kwargs)
             try:
                 result = func(*args, **kwargs)
             except Exception as e:
